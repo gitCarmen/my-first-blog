f41d4e705dd07680cd5123a7f98326a1f297ad2a
gitCarmen <285951749@qq.com>
2017-04-20 17:32:19 +0800

    Changed the HTML for the site.

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w86lcec=w8ef1#^%awz8@bk^l*v)9xt&xtmyqp)u1-jcna1h1%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
